173bdc58f9194118d8c49eb590c3b030d232fa7e
Jason Yao <jasonyao105025@gmail.com>
2023-07-07 14:48:28 -0400

    add frontend git file

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import {
+  Route,
+  Routes,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { LoginCallback, SecureRoute, Security } from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
+import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
+import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
+import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibraryPage";
+
+const oktaAuth = new OktaAuth(oktaConfig);
+
+export const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const customAuthHandler = () => {
+    navigate("/login");
+  };
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    navigate(toRelativeUrl(originalUri || "/", window.location.origin), {
+      replace: true,
+    });
+  };
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate to={"/home"} />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/search" element={<SearchBooksPage />} />
+            <Route path="/reviewlist/:bookId" element={<ReviewListPage />} />
+
+            <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
+            <Route
+              path="/login"
+              element={<LoginWidget config={oktaConfig} />}
+            />
+            <Route path="/login/callback" element={<LoginCallback />} />
+
+            {/* Need to implement Protect routes */}
+            <Route path="/shelf" element={<ShelfPage />} />
+            <Route path="/messages" element={<MessagesPage />} />
+            <Route path="/admin" element={<ManageLibraryPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Security>
+    </div>
+  );
+};
