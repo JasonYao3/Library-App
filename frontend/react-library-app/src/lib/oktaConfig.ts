2ce5b27065dad2e3ca831aea6b7c1dfd3d9e1c85
Jason Yao <jasonyao105025@gmail.com>
2024-04-15 10:39:20 -0400

    modify config files

--- frontend/react-library-app/src/lib/oktaConfig.ts
+++ frontend/react-library-app/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-  clientId: "0oaa7zoamhptuPXv05d7",
-  issuer: "https://dev-57743006.okta.com/oauth2/default",
+  clientId: "<client-id>",
+  issuer: "https://<dev-id>.okta.com",
   redirectUri: "http://localhost:3000/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: true,
