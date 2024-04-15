51047c455a3db3d7dd8b75a50ea8670db64c8f78
Jason Yao <jasonyao105025@gmail.com>
2024-04-15 14:07:37 -0400

    Create Payment intent

--- backend/spring-boot-library-app/src/main/java/com/jasonycode/springbootlibraryapp/service/PaymentService.java
+++ backend/spring-boot-library-app/src/main/java/com/jasonycode/springbootlibraryapp/service/PaymentService.java
@@ -1,10 +1,16 @@
 package com.jasonycode.springbootlibraryapp.service;
 
 import com.jasonycode.springbootlibraryapp.dao.PaymentRepository;
+import com.jasonycode.springbootlibraryapp.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.beans.factory.annotation.Value;
+
+import java.util.*;
 
 @Service
 @Transactional
@@ -12,8 +18,21 @@ public class PaymentService {
 
     private PaymentRepository paymentRepository;
 
+    @Autowired
     public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
         this.paymentRepository = paymentRepository;
         Stripe.apiKey = secretKey;
+    }
+
+    public PaymentIntent createPaymentIntent (PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("currency", paymentInfoRequest.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
     }
 }
