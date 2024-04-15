71cd703c058126e31488b0026302d1db9990d6bc
Jason Yao <jasonyao105025@gmail.com>
2024-04-15 14:11:36 -0400

    Create stripePayment

--- backend/spring-boot-library-app/src/main/java/com/jasonycode/springbootlibraryapp/service/PaymentService.java
+++ backend/spring-boot-library-app/src/main/java/com/jasonycode/springbootlibraryapp/service/PaymentService.java
@@ -1,16 +1,22 @@
 package com.jasonycode.springbootlibraryapp.service;
 
 import com.jasonycode.springbootlibraryapp.dao.PaymentRepository;
+import com.jasonycode.springbootlibraryapp.entity.Payment;
 import com.jasonycode.springbootlibraryapp.requestmodels.PaymentInfoRequest;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -34,5 +40,16 @@ public class PaymentService {
         params.put("payment_method_types", paymentMethodTypes);
 
         return PaymentIntent.create(params);
+    }
+
+    public ResponseEntity<String> stripePayment(String userEmail) throws Exception {
+        Payment payment = paymentRepository.findByUserEmail(userEmail);
+
+        if (payment == null) {
+            throw new Exception("Payment information is missing");
+        }
+        payment.setAmount(00.00);
+        paymentRepository.save(payment);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
